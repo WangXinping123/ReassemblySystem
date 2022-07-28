dd94be56169f1b9a1855f0f75074d809739ec55d
liangsenwei <2012666522@qq.com>
2022-07-28 19:31:31 +0800

    1

--- index/views.py
+++ index/views.py
@@ -5,9 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from user.models import User
 from index.models import three_pcl_img, two_pcl_img
-from index.models import three_pcl_img_patch,two_pcl_img_patch
+from index.models import three_pcl_img_patch, two_pcl_img_patch
 from django import forms
+from django.core import serializers
 from django.conf import settings
+import datetime
 
 
 # Create your views here.
@@ -46,20 +48,57 @@ def pointshow1_view(request):
 def point_upload(request):
     if request.method == "GET":
         return render(request, 'three-dimensional/pointshow.html')
-    print(request.FILES)
-    pcl_object = request.FILES.get("pcl-upload")
-    media_path = os.path.join("media", "3D_point", pcl_object.name)
-    f = open(media_path, mode='wb')
-    for chunk in pcl_object.chunks():
-        f.write(chunk)
-    f.close()
-    user = User.objects.filter(userid=request.session['userid']).first()
-    three_pcl_img.objects.create(
-        pcl_img=media_path,
-        user=user
-    )
-    queryset = three_pcl_img.objects.filter(user=request.session['userid'])
-    return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
+    # 点云文件筛选
+    elif request.method == "POST" and 'button1' in request.POST:
+        print(request.POST)
+        data1 = request.POST['data1']
+        data2 = request.POST['data2']
+        queryset = three_pcl_img.objects.filter(user=request.session['userid'],
+                                                created_time__range=(data1, data2))
+        print(data1)
+        print(queryset)
+        return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
+    # 点云文件上传
+    elif request.method == "POST" and 'point' in request.POST:
+        if request.FILES.get("pcl-upload") is None:
+            queryset = three_pcl_img.objects.filter(user=request.session['userid'])
+            return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
+        pcl_object = request.FILES.get("pcl-upload")
+        media_path = os.path.join("media", "3D_point", pcl_object.name)
+        f = open(media_path, mode='wb')
+        for chunk in pcl_object.chunks():
+            f.write(chunk)
+        f.close()
+        user = User.objects.filter(userid=request.session['userid']).first()
+        three_pcl_img.objects.create(
+            pcl_img=media_path,
+            user=user
+        )
+        queryset = three_pcl_img.objects.filter(user=request.session['userid'])
+        return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
+    # 批量点云上传
+    elif request.method == "POST" and 'lotpoint' in request.POST:
+        print("kdakdak")
+        print(request.FILES.getlist("pcl-upload"))
+        if request.FILES.getlist("pcl-upload") is None:
+            queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
+            return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
+        pcl_object = request.FILES.getlist("pcl-upload")
+        for files in pcl_object:
+            media_path = os.path.join("media", "3D_patch", files.name)
+            f = open(media_path, mode='wb')
+            for chunk in files.chunks():
+                f.write(chunk)
+            f.close()
+            user = User.objects.filter(userid=request.session['userid']).first()
+            three_pcl_img.objects.create(
+                pcl_img=media_path,
+                user=user
+            )
+        queryset = three_pcl_img.objects.filter(user=request.session['userid'])
+        print("11111")
+        print(queryset)
+        return render(request, 'three-dimensional/pointshow.html', {'queryset': queryset})
 
 
 # 碎片操作
@@ -90,20 +129,53 @@ def point_patch_show1_view(request):
 def point_patch_upload(request):
     if request.method == "GET":
         return render(request, 'three-dimensional/pointupload.html')
-    print(request.FILES)
-    pcl_object = request.FILES.get("pcl-upload")
-    media_path = os.path.join("media", "3D_patch", pcl_object.name)
-    f = open(media_path, mode='wb')
-    for chunk in pcl_object.chunks():
-        f.write(chunk)
-    f.close()
-    user = User.objects.filter(userid=request.session['userid']).first()
-    three_pcl_img_patch.objects.create(
-        pcl_patch=media_path,
-        user=user
-    )
-    queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
-    return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
+    # 点云文件筛选
+    elif request.method == "POST" and 'button1' in request.POST:
+        print(request.POST)
+        data1 = request.POST['data1']
+        data2 = request.POST['data2']
+        queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'],
+                                                      created_time__range=(data1, data2))
+        return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
+    # 点云文件上传
+    elif request.method == "POST" and 'point' in request.POST:
+        print(request.FILES.get("pcl-upload"))
+        if request.FILES.get("pcl-upload") is None:
+            queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
+            return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
+        pcl_object = request.FILES.get("pcl-upload")
+        media_path = os.path.join("media", "3D_patch", pcl_object.name)
+        f = open(media_path, mode='wb')
+        for chunk in pcl_object.chunks():
+            f.write(chunk)
+        f.close()
+        user = User.objects.filter(userid=request.session['userid']).first()
+        three_pcl_img_patch.objects.create(
+            pcl_patch=media_path,
+            user=user
+        )
+        queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
+        return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
+    # 批量点云文件上传
+    elif request.method == "POST" and 'lotpoint' in request.POST:
+        print(request.FILES.getlist("pcl-upload"))
+        if request.FILES.getlist("pcl-upload") is None:
+            queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
+            return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
+        pcl_object = request.FILES.getlist("pcl-upload")
+        for files in pcl_object:
+            media_path = os.path.join("media", "3D_patch", files.name)
+            f = open(media_path, mode='wb')
+            for chunk in files.chunks():
+                f.write(chunk)
+            f.close()
+            user = User.objects.filter(userid=request.session['userid']).first()
+            three_pcl_img_patch.objects.create(
+                pcl_patch=media_path,
+                user=user
+            )
+        queryset = three_pcl_img_patch.objects.filter(user=request.session['userid'])
+        return render(request, 'three-dimensional/pointupload.html', {'queryset': queryset})
 
 
 # ************************** 2D操作 *******************************
@@ -116,6 +188,7 @@ def point_patch_upload(request):
 def pictureupload_view(request):
     queryset = two_pcl_img_patch.objects.filter(user=request.session['userid'])
     return render(request, 'two-dimensional/pictureupload.html', {'queryset': queryset})
+
 
 # 2.删除操作
 def picture_patch_delete(request):
@@ -152,6 +225,7 @@ def picture_patch_upload(request):
     )
     queryset = two_pcl_img_patch.objects.filter(user=request.session['userid'])
     return render(request, 'two-dimensional/pictureupload.html', {'queryset': queryset})
+
 
 # 2D成品操作
 # 1.成品显示操作
